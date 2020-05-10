ce6ab5531c1535c948af7eeedc6307b82462d2fd
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2020-05-10 17:34:47 +0530

    added bg image and dashboard changes

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -7,15 +7,24 @@ import {Component, OnInit} from '@angular/core';
 })
 export class DashboardPage implements OnInit {
     slideOpts: any;
-
+    contactNo = '8005612730';
     constructor() {
         this.slideOpts = {
             initialSlide: 0,
-            speed: 400
+            slidesPerView: 1,
+            effect: 'slide',
+            speed: 1000,
+            autoplay: true,
+            loop: true,
+            updateOnWindowResize: true,
+            roundLengths: true,
+            watchOverflow: true,
         };
+
     }
 
     ngOnInit() {
     }
-
+    callUs() {
+    }
 }
